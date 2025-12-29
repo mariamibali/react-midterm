836682c5ba276a15e9598f585093d59bb2d28847
mariamibali <mariambaliashvili01@gmail.com>
2025-12-29 16:27:40 +0400

    navbar and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,25 @@
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <div className={styles.footerWrapper}>
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <Link className={styles.footerLink} href="#">
+            Conditions of Use
+          </Link>
+          <Link className={styles.footerLink} href="#">
+            Privacy Notice
+          </Link>
+          <Link className={styles.footerLink} href="#">
+            Interest-Based Ads
+          </Link>
+        </div>
+        <div>
+          <p>© 1996-2024, Amazon.com, Inc. or its affiliates</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,46 @@
 "use client";
+import { usePathname } from "next/navigation";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 
-function Navbar() {
+const NAV_LIST = [
+  {
+    id: 1,
+    title: "Products",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Profile",
+    url: "/profile",
+  },
+  {
+    id: 3,
+    title: "Cart",
+    url: "/cart",
+  },
+];
+const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <Link href="/">Home</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/cart">Cart</Link>
-      </ul>
-    </nav>
+    <div className={styles.nav}>
+      <div className={styles.navbar}>
+        {NAV_LIST.map((item) => (
+          <div key={item.id} className={styles.navList}>
+            <Link href={item.url}>
+              <h2
+                className={
+                  pathname === item.url ? styles.activeLink : styles.link
+                }
+              >
+                {item.title}
+              </h2>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default Navbar;
